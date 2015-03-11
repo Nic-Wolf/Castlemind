f480d20afe6034b6a470de9c629c5fec63acf10b
Nic Wolf <azalraku@gmail.com>
2015-03-11 15:20:26 -0700

    sending squares to the view layer

--- castleExpress/routes/index.js
+++ castleExpress/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+var express    = require('express');
+var router     = express.Router();
+var squareGrid = require('../makeSquares.js');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Castlemind Main View' });
+
+	squareGrid.completeBoard(function(squares) {
+
+		res.render('index', { 
+			title: 'Castlemind Main View', 
+			squares: squares,
+		});
+	});
+
 });
 
 module.exports = router;
+
