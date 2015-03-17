3e34db3db8f86653996384c5ab4701f342207f07
David Hasenjaeger <davehasen@gmail.com>
2015-03-17 16:09:42 -0700

    many auxiliary functions moved to their own module

--- client/index.js
+++ client/index.js
@@ -1,3 +1,5 @@
+var manageState = require('./manageState.js');
+
 var gameApp = angular.module('gameApp', ['ngCookies']);
 
 gameApp.controller('gameController', ['$http', '$cookies', function($http, $cookies) {
@@ -29,7 +31,7 @@ gameApp.controller('gameController', ['$http', '$cookies', function($http, $cook
 	// ******************************************************* //
 	function setSquares (data) {
 		$cookies.playing = true;
-		stringState(data, function (state, solution) {
+		manageState.stringState(data, function (state, solution) {
 			$cookies.state = state;
 			$cookies.solution = solution;
 		});
@@ -68,7 +70,8 @@ gameApp.controller('gameController', ['$http', '$cookies', function($http, $cook
 
 		if (self.moves.length === 5) {
 
-			resetGuess(self.moves, self.hints, self.squares, self.solution,
+			manageState.resetGuess(
+				self.moves, self.hints, self.squares, self.solution,
 				function(moves, hints, squares, message) {
 
 				self.moves = moves;
@@ -81,7 +84,8 @@ gameApp.controller('gameController', ['$http', '$cookies', function($http, $cook
 
 	function init () {
 		if ($cookies.playing) {
-			deStringState($cookies.state, $cookies.solution, function (squares, solution) {
+			manageState.deStringState(
+				$cookies.state, $cookies.solution, function (squares, solution) {
 				setSquares({"board": squares, "path": solution});
 			});
 		}
@@ -89,70 +93,3 @@ gameApp.controller('gameController', ['$http', '$cookies', function($http, $cook
 
 	init();
 }]);// end gameController
-
-function stringState (data, callback) {
-	var squares = data.board.reduce( function (prev, curr) {
-		return prev + curr.colorKey;
-	}, '');
-	var solution = data.path.reduce( function (prev, curr, ind) {
-		if (ind < data.path.length - 1) {
-			return prev + curr.index + curr.direction + curr.solution + '#';
-		} else {
-			return prev + curr.index;
-		}
-	}, '');
-	callback(squares, solution);
-}
-
-function deStringState (boardString, solutionString, callback) {
-	var board = [];
-	var boardArray = boardString.split('');
-	boardArray.forEach(function (elem, ind) {
-		var square = {};
-		if (ind < 25) {
-			square.colorKey = Number(elem);
-			square.value = [Math.floor(ind / 5), ind % 5];
-			board.push(square);
-		}
-	});
-	var solution = [];
-	var solutionArray = solutionString.split('#');
-	solutionArray.forEach( function (elem, ind) {
-		console.log(elem);
-		if (ind < solutionArray.length - 1) {
-			var newMove = {};
-			newMove.direction = elem.match(/[^\d]+/)[0];
-			newMove.index = Number(elem.slice(0,elem.match(/[^\d]+/).index));
-			newMove.solution = Number(elem.slice(-1));
-		} else {
-			var newMove = {};
-			newMove.index = Number(elem);
-		}
-		console.log(newMove);
-		solution.push(newMove);
-	});
-	callback(board, solution);
-}
-
-// resetGuess displays what part of the guess was correct.
-// If the guess is only partially correct, it resets the guess.
-function resetGuess (moves, hints, squares, solution, callback) {
-	squares.forEach( function (elem, ind) {
-		if (elem.class.indexOf('clicked') !== -1) {
-			squares[ind].class = elem.class.split(' ').slice(0, 2).join(' ');
-		}
-	});
-	if (!moves.some( function (elem, ind) {
-		var result = elem.value !== solution[ind].solution;
-		if (!result) {
-			hints[ind].class = squares[solution[ind + 1].index].class;
-		}
-		return result;
-	})) {
-		message = "You win!";
-	} else {
-		moves = [];
-		message = "Keep trying!";
-	}
-	callback(moves, hints, squares, message);
-}// end resetGuess
