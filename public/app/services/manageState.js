d3d325313a4843664a60090a83cd45d0026554a4
My Name <aaronollis121@gmail.com>
2015-03-25 14:44:30 -0700

    path hints now has glow

--- public/app/services/manageState.js
+++ public/app/services/manageState.js
@@ -108,7 +108,7 @@ function resetGuess (moves, hints, squares, solution, guesses, results, callback
 //		class: the chosen color
 //		value: the associated number
 //	...modifies the hits array so that
-//		only the current hint has the class currentMove
+//		only the current hint has the class glow
 //	...modifies the following properties of the active square
 //		class: hasImage added, clicked added (only if its not first or last)
 //		imgClass: ng-hide removed
@@ -121,10 +121,10 @@ function incrementMoves (square, hints, moves, squares, callback) {
 
 	hints = hints.map(function(elem) {
 		var result = elem;
-		result.class = elem.class.split(' currentMove').join('');
+		result.class = elem.class.split(' glow').join('');
 		return result;
 	});
-	hints[moves.length].class += ' currentMove';
+	hints[moves.length].class += ' glow';
 
 	moves.push(move);
 	if (square.class.indexOf(' b') === -1 && square.class.indexOf(' a') === -1) {
