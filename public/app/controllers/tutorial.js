9428e0de37628318c22fb491f40e4cea2f22688a
David Hasenjaeger <davehasen@gmail.com>
2015-03-27 12:19:34 -0700

    first two slides of the tutorial complete

--- public/app/controllers/tutorial.js
+++ public/app/controllers/tutorial.js
@@ -57,16 +57,16 @@ tutApp.controller('tutorialController', ['$location', '$timeout',
 		'Your objective is to guess the path that the computer took to the castle.',
 		'Click next to start the tutorial.',
 		'You can see the start and end points.',
-		"You can also see hints about the computer's moves",
-		"Before each move, the possible next moves flash.",
-		"Each time you click, the board updates to show your progress.",
+		"You can also see hints about the computer's moves.",
 		"Click next to see an example of a guess.",
+		"Before each move, the possible next moves flash.",
+		"Each time you click, the board and user moves update to show your progress.",
 		"First click: pink",
 		"Second click: blue",
 		"Third click: red",
 		"Fourth click: green",
-		"Fifth click: castle",
-		"That was the first guess."
+		"After the fifth click, the guess is complete.  The clicked squares are cleared and the hints update to show which squares you got correct.",
+		"There are no more slides."
 	];
 
 	// state which message is the oldest to be displayed and which messages are
@@ -81,6 +81,8 @@ tutApp.controller('tutorialController', ['$location', '$timeout',
 		result.imgClass = "ng-hide";
 		return result;
 	});
+	this.hints = exampleSolution.map(manageState.makeHint);
+	this.hints.class = '';
 	this.solution = exampleSolution;
 
 	// give the start and end the correct styling
@@ -89,6 +91,9 @@ tutApp.controller('tutorialController', ['$location', '$timeout',
 	this.example[12].image = "../assets/img/o.png";
 	this.example[6].class += ' b';
 
+	this.moves = [{
+		"class": 'square color-4 a'
+	}];
 
 	var path = [11, 16, 19, 1,6];
 
@@ -100,33 +105,44 @@ tutApp.controller('tutorialController', ['$location', '$timeout',
 		bePatient
 	];
 
+	this.buttonClass = ' highlight';
 	this.next = function () {
 		slides[slideNumber](0);
+		this.buttonClass = this.buttonClass.split(' highlight').join('');
 		slideNumber = slides.length - 1;
 	};
 
 	// declare what will be highlighted after each direction is introduced
 	var drawAttention = [
-		[this.example[6], this.example[12]]
+		[this.example[6], this.example[12]],
+		[this.hints]
 	];
 
 	var self = this;
 
 	function gameRules (num) {
+		if (num > 0) {
+			drawAttention[num - 1].forEach( function (elem) {
+				elem.class = elem.class.split(' highlight').join('');
+			});
+		}
+		if (num < drawAttention.length) {
+			drawAttention[num].forEach( function (elem) {
+				elem.class += ' highlight';
+			});
+		}
 		self.messages.push(messages[self.messages.length]);
 		if (self.messages.length > 5) {
 			self.startMessages++;
 		}
 		num++;
-		if (num <= 4) {
+		if (num <= drawAttention.length) {
 			$timeout( function () {
 				gameRules(num);
 			},2000);
 		} else {
-			legalMoves[0].forEach( function (elem) {
-				self.example[elem].class += ' highlight';
-			});
 			slideNumber = 1;
+			self.buttonClass = ' highlight';
 		}
 	}
 
@@ -143,27 +159,51 @@ tutApp.controller('tutorialController', ['$location', '$timeout',
 	function firstGuess (num) {
 		self.messages.push(messages[self.messages.length]);
 		self.startMessages++;
-		legalMoves[num].forEach( function (elem) {
-			self.example[elem].class = self.example[elem].class.split(' highlight').join('');
-		});
-		if (self.example[path[num]].class.indexOf(' b') === -1) {
-			self.example[path[num]].class += ' hasImage';
-			self.example[path[num]].imgClass = "";
-			var string = self.solution[num + 1].direction;
-			var hint = string.split(' ').reduce(function (prev, curr) {return prev + curr[0];}, '');
-			self.example[path[num]].image = "../assets/img/" + hint + ".png";
-			legalMoves[num + 1].forEach( function (elem) {
+		if (num === 0) {
+			legalMoves[num].forEach( function (elem) {
 				self.example[elem].class += ' highlight';
 			});
-			num++;
-			if (num < path.length) {
-				$timeout(function () {
-					firstGuess(num);
-				}, 2000);
+		} else if (num > 1 && num < path.length + 2) {
+			legalMoves[num - 2].forEach( function (elem) {
+				self.example[elem].class = self.example[elem].class.split(' highlight').join('');
+			});
+			if (self.example[path[num - 2]].class.indexOf(' b') === -1) {
+				self.moves.push({
+					"class": self.example[path[num - 2]].class
+				});
+				self.example[path[num - 2]].class += ' hasImage';
+				self.example[path[num - 2]].imgClass = "";
+				var string = self.solution[num - 1].direction;
+				var hint = string.split(' ').reduce(function (prev, curr) {return prev + curr[0];}, '');
+				self.example[path[num - 2]].image = "../assets/img/" + hint + ".png";
+				legalMoves[num - 1].forEach( function (elem) {
+					self.example[elem].class += ' highlight';
+				});
 			} else {
-				self.messages.push(messages[self.messages.length]);
-				self.startMessages++;
+				self.moves = self.moves.slice(0,1);
+				path.forEach( function (elem) {
+					var square = self.example[elem];
+					square.class = square.class.split(' hasImage').join('');
+					square.imgClass = "ng-hide";
+					delete square.image;
+					self.example[elem] = square;
+				});
+				self.hints[1].class += ' color-3';
+				self.hints.class = ' highlight';
 			}
+		}
+		num++;
+		if (num < path.length + 2) {
+			$timeout(function () {
+				firstGuess(num);
+			}, 2000);
+		} else if (num === path.length + 2) {
+			$timeout(function () {
+				firstGuess(num);
+			}, 4000);
+		} else {
+			self.hints.class = '';
+			slideNumber = 2;
 		}
 	}
 
