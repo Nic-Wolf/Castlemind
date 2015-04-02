b3b217d4d4874bd4fa42fc4bc796b98811b76ded
David Hasenjaeger <davehasen@gmail.com>
2015-04-02 14:30:49 -0700

    tutorial is streamlined

--- public/app/controllers/tutorial.js
+++ public/app/controllers/tutorial.js
@@ -55,34 +55,12 @@ tutApp.controller('tutorialController', ['$location', '$timeout',
 	// This is a complete list of messages for the tutorial
 	var messages = [
 		'',
-		'Your objective is to guess the path that the computer took to the castle.',
-		'Click next to start the tutorial.',
-		'Here are the start and end points.',
-		"These are hints about the computer's moves.",
-		"In this example, you can see all four movement types:",
-		'one space horizontal or vertical',
-		'one space diagonal',
-		'three spaces horizontal or vertical',
-		'three spaces diagonal',
-		"There are always five moves in one game.",
-		"Let's see what a guess looks like.",
-		"Before each move, the possible next moves flash.",
-		"First click: desert.  Notice the pink square in the user moves bar?",
-		"Second click: valley.",
-		"Third click: desert.",
-		"Fourth click: desert.",
-		"The castle is the last click.  Now the guess is complete.",
-		"The hints update after a guess to show which squares you got correct.",
-		"Click next to see the solution.",
-		"The first square must be desert, but there are two desert squares.",
-		"Let's try the one we didn't use last time.",
-		"Second click: forest",
-		"Third click: desert",
-		"Fourth click: desert",
-		"The last click is the castle.",
-		"This time the hints fill up all the way.",
-		"The board didn't reset either.",
-		"That was the correct guess! That's all there is to it."
+		'Your job: Figure out the secret path that leads to the castle in five moves.',
+		"You get hints about the secret path, but there may be several paths using these moves.",
+		"Choices like this one will leave you stuck.  You will have to guess again.",
+		"Many paths will get you there, like the one below.  But this isn't the secret path either.",
+		"After each guess you see how close you got.  In this case the first square is a desert.",
+		"This time, the guess is the secret path. Remember, you only win if you find the secret path!"
 	];
 
 	// this.messages starts with only one message
@@ -122,28 +100,16 @@ tutApp.controller('tutorialController', ['$location', '$timeout',
 	// prompt the user to start the tutorial after two seconds
 	$timeout( function () {
 		addToMessages();
+		viewNumber = 0;
 	}, 0);
-	$timeout( function () {
-		addToMessages();
-		slideNumber = 0;
-	}, 2000);
 
 
 	// set up a function to represent each click of the next button
 	// there are three "slides" and each slide has several "views"
-	var slides = [
-		gameRules,
-		firstGuess,
-		correctGuess,
-		bePatient
-	];
-	var slideNumber = slides.length - 1;
-	var viewNumber = 0;
-	var timesClicked = 0;
+	var viewNumber = 5;
 
 	this.next = function () {
-		slides[slideNumber](viewNumber);
-		timesClicked++;
+		nextView(viewNumber);
 	};
 
 
@@ -154,176 +120,85 @@ tutApp.controller('tutorialController', ['$location', '$timeout',
 	// first slide
 	// ******************************************************************** //
 
-	// declare what will be highlighted after each direction is introduced
-	var drawAttention = [
-		[this.example[6], this.example[12]],
-		[this.hints],
-		[this.hints],
-		[this.hints[0]],
-		[this.hints[1]],
-		[this.hints[2]],
-		[this.hints[3]],
-		[this.hints]
-	];
-
-	function gameRules (num) {
-		if (num > 0) {
-			drawAttention[num - 1].forEach( function (elem) {
-				elem.class = elem.class.split(' highlight').join('');
-			});
-		}
-		if (num < drawAttention.length) {
-			drawAttention[num].forEach( function (elem) {
-				elem.class += ' highlight';
-			});
-		}
-		addToMessages();
-		if (self.messages.length > 5) {
-		}
-		if (num < drawAttention.length) {
-			viewNumber++;
-		} else {
-			slideNumber = 1;
-			viewNumber = 0;
-		}
-	}// end gameRules()
-
-
-	// ******************************************************************** //
-	// second slide
-	// ******************************************************************** //
-
 	// declare what squares are legal after each click and what the clicks are
-	var legalMovesGuess = [
-		[7, 11, 13, 17],
-		[5, 7, 15, 17],
-		[0, 18],
-		[0],
-		[6],
-		[]
-	];
-	var pathGuess = [11, 15, 18, 0, 6];
+	var pathStuck = [11, 17, 2];
+	var pathGuess = [11, 15, 18, 0];
+	var pathCorrect = [7, 3, 18, 0];
 
-	function firstGuess (num) {
+	function nextView (num) {
 		addToMessages();
-		if (num < 5) {
-			click(num - 1, pathGuess, legalMovesGuess);
-		}
-
-		if (num === 5) {
-			self.moves = self.moves.slice(0,1);
-			pathGuess.forEach( function (elem) {
-				var square = self.example[elem];
-				square.class = square.class.split(' hasImage').join('');
-				square.imgClass = "ng-hide";
-				delete square.image;
-				self.example[elem] = square;
-			});
-			self.hints[1].class += ' color-3';
-			self.example[6].class += ' highlight';
-		} 
-
-		if (num === 6) {
-			self.example[6].class = self.example[6].class.split(' highlight').join('');
-			self.hints[1].class += ' highlight';
-		}
-
-		if (num < 7) {
-			viewNumber++;
-		} else {
-			self.hints[1].class = self.hints[1].class.split(' highlight').join('');
-			viewNumber = 0;
-			slideNumber = 2;
-		}
-	}// end firstGuess()
-
-
-	// ******************************************************************** //
-	// third slide
-	// ******************************************************************** //
-
-	// declare what squares are legal after each click
-	var legalMovesCorrect = [
-		[7, 11],
-		[1, 11, 3, 13],
-		[0, 18],
-		[0],
-		[6],
-		[]
-	];
-	var pathCorrect = [7, 3, 18, 0, 6];
-
-	function correctGuess (num) {
-		addToMessages();
-		if (num < 5) {
-			click(num - 1, pathCorrect, legalMovesCorrect);
-		}
-
-		if (num === 5) {
+		if (num === 0) {
+			self.hints.class += ' highlight';
+		} else if (num === 1) {
+			self.hints.class = '';
+			var n;
+			for (n = 0; n < 3; n++) {
+				click(n, pathStuck);
+			}
+		} else if (num === 2) {
+			clearPath(pathStuck);
+			for (n = 0; n < 4; n++) {
+				click(n, pathGuess);
+			}
 			self.moves.push({"class": 'square b'});
-			self.example[6].class = self.example[6].class.split(' highlight').join('');
+		} else if (num === 3) {
+			clearPath(pathGuess);
+			self.hints[1].class += ' color-3 highlight';
+			[7,11].forEach( function (elem) {
+				self.example[elem].class += ' highlight';
+			});
+		} else if (num === 4) {
+			self.hints[1].class = self.hints[1].class.split(' highlight').join('');
+			[7,11].forEach( function (elem) {
+				self.example[elem].class = self.example[elem].class.split(' highlight').join('');
+			})
+			var n;
+			for (n = 0; n < 4; n++) {
+				click(n, pathCorrect);
+			}
+			self.moves.push({"class": 'square b'});
 			self.hints.slice(1,5).forEach( function (elem, ind) {
 				var square = self.example[exampleSolution[ind + 1].index];
 				elem.class = square.class;
 			});
 		}
 
-		if (num === 6) {
-			self.hints.class += ' highlight';
-		}
 
-		if (num === 7) {
-			self.hints.class = self.hints.class.split(' highlight').join('');
-			pathCorrect.forEach( function (elem) {
-				self.example[elem].class += ' highlight';
-			});
-		}
-
-		if (num === 8) {
-			pathCorrect.forEach( function (elem) {
-				self.example[elem].class = self.example[elem].class.split(' highlight').join('');
-			});
-		}
-
-		if (num < 8) {
+		if (num <= 4) {
 			viewNumber++;
 		} else {
-			viewNumber = 0;
-			slideNumber = 3;
+			// this is the end of the tutorial
 		}
-	}// end correctGuess()
+
+	}// end nextView()
 
 
 	// ******************************************************************* //
 	// support functions
 	// ******************************************************************* //
 
-	function bePatient () {
-		// This is nothing
+	// clearPath removes the styling that's added by a click
+	function clearPath (path) {
+		self.moves = self.moves.slice(0,1);
+		path.forEach( function (elem) {
+			var square = self.example[elem];
+			square.class = square.class.split(' hasImage').join('');
+			square.imgClass = "ng-hide";
+			delete square.image;
+			self.example[elem] = square;
+		});
 	}
 
-	function click (number, path, legalMoves) {
-		if (number < 0) {
-			legalMoves[0].forEach( function (elem) {
-				self.example[elem].class += ' highlight';
-			});
-		}
 
-		if (number >= 0) {
-			legalMoves[number].forEach( function (elem) {
-				self.example[elem].class = self.example[elem].class.split(' highlight').join('');
-			});
-			self.moves.push({
-				"class": self.example[path[number]].class
-			});
-			self.example[path[number]].class += ' hasImage';
-			self.example[path[number]].imgClass = "";
-			var string = self.solution[number + 1].direction;
-			var hint = string.split(' ').reduce(function (prev, curr) {return prev + curr[0];}, '');
-			self.example[path[number]].image = "../assets/img/" + hint + ".png";
-			legalMoves[number + 1].forEach( function (elem) {
-				self.example[elem].class += ' highlight';
-			});
-		}
+	// click mimics the user clicking on a square
+	function click (number, path) {
+		self.moves.push({
+			"class": self.example[path[number]].class
+		});
+		self.example[path[number]].class += ' hasImage';
+		self.example[path[number]].imgClass = "";
+		var string = self.solution[number + 1].direction;
+		var hint = string.split(' ').reduce(function (prev, curr) {return prev + curr[0];}, '');
+		self.example[path[number]].image = "../assets/img/" + hint + ".png";
 	}
 }]);
