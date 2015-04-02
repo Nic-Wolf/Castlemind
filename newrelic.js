40f58a99dd05e83a934877a5340f5e2a55387913
Nic Wolf <azalraku@gmail.com>
2015-04-02 14:00:39 -0700

    added new relic's apm

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['castlemind'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '2a4fcf8febb5b3d46dec9954f17367b482bee29e',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
